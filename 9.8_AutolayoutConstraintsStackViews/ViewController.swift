10dc3aa05f87b09f912f4ceee0fb7b1cd8882c6c
Alexandre Samson <alekssamson@gmail.com>
2021-05-13 17:44:57 +0200

    Initial Commit

---
+++ 9.8_AutolayoutConstraintsStackViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9.8_AutolayoutConstraintsStackViews
+//
+//  Created by Alexandre Samson on 13.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 9.8_AutolayoutConstraintsStackViewsTests/__8_AutolayoutConstraintsStackViewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  __8_AutolayoutConstraintsStackViewsTests.swift
+//  9.8_AutolayoutConstraintsStackViewsTests
+//
+//  Created by Alexandre Samson on 13.05.21.
+//
+
+import XCTest
+@testable import __8_AutolayoutConstraintsStackViews
+
+class __8_AutolayoutConstraintsStackViewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
